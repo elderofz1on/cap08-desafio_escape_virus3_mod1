869c5b5566034776ec9232f9823ea971f693ad15
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 17:00:10 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let jogador = game.createSprite(2, 4)
+let virus = game.createSprite(randint(0, 4), 0)
+game.setScore(0)
+basic.forever(function () {
+	
+})
